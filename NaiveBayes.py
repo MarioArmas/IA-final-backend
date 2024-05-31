4e780e2ac1f4ccfb2782b048ff2b6a525f137450
DiegoBautista <dabautistac@correo.url.edu.gt>
2024-05-30 23:27:34 -0600

    Modificaciones el diccionario de palabras para ignorar

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -4,23 +4,13 @@ rotten = []
 fresh = []
 
 common_word = [
-    "the",
-    "be",
-    "of",
-    "and",
-    "a",
-    "in",
-    "that",
-    "have", 	
-    "I",
-    "it",
-    "for",
-    "not",
-    "on",
-    "with",
-    "he",
-    "Pron"
-]
+    "the", "be", "to", "of", "and", "a", "in", "that", "have", "I",
+    "it", "for", "not", "on", "with", "he", "as", "you", "do", "at",
+    "this", "but", "his", "by", "from", "they", "we", "say", "her",
+    "she", "or", "an", "will", "my", "one", "all", "would", "there",
+    "their", "what", "so", "up", "out", "if", "about", "who", "get",
+    "which", "go", "me", "i", "pron"
+    ]
 
 # Tabla de frecuencias (Bag of words)
 def crear_tabla_frecuencias(corpus):
